f9bcc9a17a3863db28f2f1212d5bb80992d436e1
Lobster <nazar.himin@gmail.com>
2020-02-23 20:40:20 +0200

    fix book grid

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -16,19 +16,14 @@ $(function() {
 });
 
 $(window).scroll(function() {
-  if ($(this).scrollTop() >= 50) {
-    // If page is scrolled more than 50px
-    $('#return-to-top').fadeIn(200); // Fade in the arrow
+  if ($(this).scrollTop() > 300) {
+    $('#return-to-top').addClass('show');
   } else {
-    $('#return-to-top').fadeOut(200); // Else fade out the arrow
+    $('#return-to-top').removeClass('show');
   }
 });
-$('#return-to-top').click(function() {
-  // When arrow is clicked
-  $('body,html').animate(
-    {
-      scrollTop: 0 // Scroll to top of body
-    },
-    500
-  );
+
+$('#return-to-top').on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({ scrollTop: 0 }, '300');
 });
